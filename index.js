a359e24f0bbb07ba3b4f923e69c08b02e0045601
odabe <odabe.crystal@gmail.com>
2018-08-29 13:38:19 -0700

    This is the first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Function for api getData//
+function getDataFromApi(){
+}
+
+//Render the searchbar and form with responsive button
+const searchPage= []
+//
+
+// function and event listeners to switch between pages: search page and favorites page//
+function startSearch(){
+
+}
+// Render Results function//
+function renderResults(result){
+}
+// function for dislike/"no,thanks" and favorites button//
+
+//button that takes user to favorites //
+
+//button that takes you back to search page//
+
+
