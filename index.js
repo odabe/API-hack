0bb230f321c21c5a982946115b6ad81ac3504d78
Crystal Odabe <38960424+odabe@users.noreply.github.com>
2018-08-30 10:02:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,40 @@ function getDataFromApi(){
 }
 
 //Render the searchbar and form with responsive button
-const searchPage= []
+const userPage = [
+	{
+	 page: `
+    <u>Pick</u>
+    <form action="#" class="js-search-form">
+      <label for="query">
+      <input id="input" class="controls" type="text" placeholder="What to chews...">
+      <div id="map"></div>
+      </label>
+      <button type="submit" id="searchMap">Search</button>
+    </form> 
+      <br>
+      <i><u>Chews</u></i>
+      <br>
+      <button type="button" class="favs-button"> Chews'n Ones!🍴</b></button>
+        `
+	},
+  {
+	 page: `
+      <u>Chews'n Ones<u>
+      <br>
+      <button type="button" class="final"> Pick a bite!</button>
+        `
+	},
+    {
+	 page: `
+      <u>WooHoo!<u>
+      Go grab a bite!
+      <br>
+      <button type="button" class="home"><a href="index.html">Home 🏡!</b></a></button>
+        `
+	}
+];
+let pageIndex = 0;
 //
 
 // function and event listeners to switch between pages: search page and favorites page//
